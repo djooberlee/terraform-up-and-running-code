6fa1fa0bc9249880189c0f7b8a93281568a9ef31
Oleksii <alex.smesh@gmail.com>
2023-03-27 00:01:54 +0300

    using vars
    Signed-off-by: Oleksii <alex.smesh@gmail.com>

--- main.tf
+++ main.tf
@@ -3,17 +3,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
+  ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
-
-  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
@@ -22,16 +20,20 @@ resource "aws_instance" "example" {
 
 resource "aws_security_group" "instance" {
 
+#  name = "terraform-example-instance"
   name = var.security_group_name
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+variable "server_port" {
+  default     = 8080
+}
 variable "security_group_name" {
   description = "The name of the security group"
   type        = string
